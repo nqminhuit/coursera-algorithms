e979c0003c33b17b4e3f52d71f830a1e2c796f79
Minh Quoc Nguyen <nqminhuit@gmail.com>
2020-03-17 21:59:56 +0700

    init project with Percolation algorithm API

---
+++ Percolation/src/PercolationStats.java
@@ -0,0 +1,31 @@
+public class PercolationStats {
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return 0D;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return 0D;
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return 0D;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return 0D;
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+    }
+
+}
