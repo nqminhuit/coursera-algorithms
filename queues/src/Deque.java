4c21b8e5861aa8c43d2f421adff244b9c9898ea2
Minh Quoc Nguyen <nqminhuit@gmail.com>
2020-03-29 10:24:20 +0700

    - implement Queue - implement Deque methods with the combination of addFirst() and Stack.push(), addLast() and Queue.enqueue()

--- queues/src/Deque.java
+++ queues/src/Deque.java
@@ -3,13 +3,20 @@ import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
 
+    private Node first;
+
+    private Node last;
+
     // construct an empty deque
     public Deque() {
+        first = new Node(null);
+        last = first;
+        first.next = last;
     }
 
     // is the deque empty?
     public boolean isEmpty() {
-        return false;
+        return first.item == null && last.item == null;
     }
 
     // return the number of items on the deque
@@ -26,11 +33,13 @@ public class Deque<Item> implements Iterable<Item> {
     // add the item to the front
     public void addFirst(Item item) {
         validateItem(item);
+        push(item);
     }
 
     // add the item to the back
     public void addLast(Item item) {
         validateItem(item);
+        enqueue(item);
     }
 
     private void preventOperationOnEmptyQueue() {
@@ -42,27 +51,79 @@ public class Deque<Item> implements Iterable<Item> {
     // remove and return the item from the front
     public Item removeFirst() {
         preventOperationOnEmptyQueue();
-        return null;
+        return dequeue();
     }
 
     // remove and return the item from the back
     public Item removeLast() {
         preventOperationOnEmptyQueue();
-        return null;
+        return pop();
     }
 
-    private class ListItorator<Item> implements Iterator<Item> {
+    private class Node {
+
+        Item item;
+
+        Node next;
+
+        public Node(Item item) {
+            this.item = item;
+        }
+    }
+
+    private void initializeFirstItem(Item item) {
+        last.item = item;
+        first.item = item;
+    }
+
+    private void enqueue(Item item) {
+        if (isEmpty()) {
+            initializeFirstItem(item);
+        } else {
+            Node newLast = new Node(item);
+            last.next = newLast;
+            last = newLast;
+        }
+    }
+
+    private Item dequeue() {
+        Item item = first.item;
+        first = first.next;
+        return item;
+    }
+
+    private void push(Item item) {
+        if (isEmpty()) {
+            initializeFirstItem(item);
+        } else {
+            Node newFirst = new Node(item);
+            newFirst.item = item;
+            newFirst.next = first;
+            first = newFirst;
+        }
+    }
+
+    private Item pop() {
+        Item item = last.item;
+        last = last.next;
+        return item;
+    }
+
+    private class ListItorator implements Iterator<Item> {
+
+        Node current = first;
 
         @Override
         public boolean hasNext() {
-            // TODO Auto-generated method stub
-            return false;
+            return current != null;
         }
 
         @Override
         public Item next() {
             preventOperationOnEmptyQueue();
-            return null;
+            Item item = current.item;
+            current = current.next;
+            return item;
         }
 
         @Override
@@ -74,11 +135,19 @@ public class Deque<Item> implements Iterable<Item> {
 
     // return an iterator over items in order from front to back
     public Iterator<Item> iterator() {
-        return null;
+        return new ListItorator();
     }
 
     // unit testing (required)
     public static void main(String[] args) {
+        Deque<String> deque = new Deque<>();
+        for (int i = 0; i < 3; ++i) {
+            deque.addFirst("item " + i);
+            deque.addLast("item " + i);
+        }
+        for (String item : deque) {
+            System.out.println(item);
+        }
     }
 
 }
