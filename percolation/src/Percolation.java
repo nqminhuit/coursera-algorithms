4d58f667f9c81bc03026d95bde7691ca4d6a8b85
Minh Quoc Nguyen <nqminhuit@gmail.com>
2020-03-29 09:00:04 +0700

    - introduce new WQUF instance for checking isFull() - reduce calls to WQUF by introducing if statement.

--- percolation/src/Percolation.java
+++ percolation/src/Percolation.java
@@ -9,6 +9,8 @@ public class Percolation {
 
     private final WeightedQuickUnionUF wquf;
 
+    private final WeightedQuickUnionUF wqufFullCheck;
+
     private int numberOfOpenSites = 0;
 
     private final int virtualBot;
@@ -22,13 +24,14 @@ public class Percolation {
         }
         this.n = n;
         openSites = new boolean[n + 1][n + 1];
-        wquf = new WeightedQuickUnionUF((n + 1) * (n + 1) + 2);
-        virtualBot = n * n + 1;
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= n; ++j) {
                 openSites[i][j] = false;
             }
         }
+        wquf = new WeightedQuickUnionUF((n + 1) * (n + 1) + 2);
+        wqufFullCheck = new WeightedQuickUnionUF((n + 1) * (n + 1) + 1);
+        virtualBot = n * n + 1;
     }
 
     private int getIndex(int row, int col) {
@@ -44,19 +47,24 @@ public class Percolation {
         int toBeOpenedSite = getIndex(row, col);
         if (row > 1 && isOpen(row - 1, col)) {
             wquf.union(toBeOpenedSite, getIndex(row - 1, col));
+            wqufFullCheck.union(toBeOpenedSite, getIndex(row - 1, col));
         }
         if (row < n && isOpen(row + 1, col)) {
             wquf.union(toBeOpenedSite, getIndex(row + 1, col));
+            wqufFullCheck.union(toBeOpenedSite, getIndex(row + 1, col));
         }
         if (col > 1 && isOpen(row, col - 1)) {
             wquf.union(toBeOpenedSite, getIndex(row, col - 1));
+            wqufFullCheck.union(toBeOpenedSite, getIndex(row, col - 1));
         }
         if (col < n && isOpen(row, col + 1)) {
             wquf.union(toBeOpenedSite, getIndex(row, col + 1));
+            wqufFullCheck.union(toBeOpenedSite, getIndex(row, col + 1));
         }
 
         if (row == 1) {
             wquf.union(toBeOpenedSite, VIRTUAL_TOP);
+            wqufFullCheck.union(toBeOpenedSite, VIRTUAL_TOP);
         } else if (row == n) {
             wquf.union(toBeOpenedSite, virtualBot);
         }
@@ -65,17 +73,24 @@ public class Percolation {
         numberOfOpenSites++;
     }
 
-    // is the site (row, col) open?
-    public boolean isOpen(int row, int col) {
+    private void validateOutsideOfRange(int row, int col) {
         if (row > n || col > n || row < 1 || col < 1) {
             throw new IllegalArgumentException("row/col outside of range!");
         }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        validateOutsideOfRange(row, col);
         return openSites[row][col];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        return isOpen(row, col) && wquf.find(getIndex(row, col)) == wquf.find(VIRTUAL_TOP);
+        if (isOpen(row, col)) {
+            return wqufFullCheck.find(getIndex(row, col)) == wqufFullCheck.find(VIRTUAL_TOP);
+        }
+        return false;
     }
 
     // returns the number of open sites
