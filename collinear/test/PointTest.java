08814b26c9756b9a3bcb2b4fe733279ba7f48aa9
Minh Quoc Nguyen <nqminhuit@gmail.com>
2020-04-02 19:37:07 +0700

    - implement slopeTo() and compareTo() - add unit tests

--- collinear/test/PointTest.java
+++ collinear/test/PointTest.java
@@ -1,15 +1,46 @@
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class PointTest {
 
+    private static final double EPSILON = 0.000001D;
+
     @Test
-    public void pointTest() {
+    public void slopeTo() {
         // given:
-        Point point = new Point(1, 1);
+        Point point = new Point(6, 1);
+        Point secondPoint = new Point(4, 2);
 
         // when:
-        // point.slopt
+        double slope = point.slopeTo(secondPoint);
 
         // then:
+        assertEquals(-0.5D, slope, EPSILON);
+    }
+
+    @Test
+    public void compareY() {
+        // given:
+        Point point = new Point(6, 1);
+        Point secondPoint = new Point(4, 2);
+
+        // when:
+        int compare = point.compareTo(secondPoint);
+
+        // then:
+        assertEquals(-1, compare);
+    }
+
+    @Test
+    public void compareX() {
+        // given:
+        Point point = new Point(6, 2);
+        Point secondPoint = new Point(4, 2);
+
+        // when:
+        int compare = point.compareTo(secondPoint);
+
+        // then:
+        assertEquals(2, compare);
     }
 }
