7255535b8964eb426d072c63b38e97b54f1f7be6
Minh Quoc Nguyen <nqminhuit@gmail.com>
2020-04-02 19:08:07 +0700

    - add API test, commandline

---
+++ collinear/test/PointTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void pointTest() {
+        // given:
+        Point point = new Point(1, 1);
+
+        // when:
+        // point.slopt
+
+        // then:
+    }
+}
