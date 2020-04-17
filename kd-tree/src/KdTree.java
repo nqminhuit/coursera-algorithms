217adc5cc89bcfd965afb90270f3e3d4211cf233
Minh Quoc Nguyen <nqminhuit@gmail.com>
2020-04-17 21:15:49 +0700

    - fix Tests for exact floating-point equality

--- kd-tree/src/KdTree.java
+++ kd-tree/src/KdTree.java
@@ -45,9 +45,9 @@ public class KdTree {
             return 0D;
         }
         if (node.level % 2 == 0) {
-            return node.point.x() == p.x() ? -1 : node.point.x() - p.x();
+            return node.point.x() - p.x() == 0D ? -1 : node.point.x() - p.x();
         }
-        return node.point.y() == p.y() ? -1 : node.point.y() - p.y();
+        return node.point.y() - p.y() == 0D ? -1 : node.point.y() - p.y();
     }
 
     // add the point to the set (if it is not already in the set)
